ac332b1e5a58fc21acb4d6652dd984000187cd55
gearule <kh.muqahwi@gmail.com>
2026-04-26 17:20:00 +0300

    Fabric port of CleanSwing for Minecraft 26.1.2

---
+++ src/main/java/net/gear/cleanswing/CleanSwing.java
@@ -0,0 +1,11 @@
+package net.gear.cleanswing;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CleanSwing implements ModInitializer {
+	public static final String MOD_ID = "cleanswing";
+
+	@Override
+	public void onInitialize() {
+	}
+}
